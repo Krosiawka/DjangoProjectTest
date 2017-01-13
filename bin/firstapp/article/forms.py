ec6a01bf1bc36492860eb4c6fb43fdc6a345553c
kros <v_paliy@inbox.ru>
2017-01-13 10:33:30 +0300

    final migrate on Material Design Lite

--- bin/firstapp/article/forms.py
+++ bin/firstapp/article/forms.py
@@ -2,8 +2,12 @@
 
 from django.forms import ModelForm
 from article.models import Comments
+from django import forms
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comments
         fields = ['comments_text']
+        widgets = {
+            'comments_text': forms.Textarea(attrs={"class": "mdl-textfield__input", "type": "text", "rows": "5", "id": "sample5"}),          
+        }
